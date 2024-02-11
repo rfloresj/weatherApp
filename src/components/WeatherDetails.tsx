82a69d99757bb98d89ec3e50b8f9386cd77c9d65
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-11 10:44:45 -0600

    interfaces:WeatherDetails

--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React from "react";
+import { LuEye } from "react-icons/lu";
 
-type Props = {}
+export interface WeatherDetailsProps {
+  visibility: string;
+  humidity: string;
+  windSpeed: string;
+  airPressure: string;
+  sunrise: string;
+  sunset: string;
+}
 
-const WeatherDetails = (props: Props) => {
+export default function WeatherDetails({}: Props) {
   return (
-    <>WeatherDetails</>
-  )
+    <>
+      <SingleWeatherDetail icon={<LuEye />} information="Visibility" value={} />
+    </>
+  );
 }
 
 export interface SingleWeatherDetailProps {
-    information: string;
-    icon: React.ReactNode;
-    value: string;
+  information: string;
+  icon: React.ReactNode;
+  value: string;
 }
 
-function SingleWeatherDetail(props: SingleWeatherDetailProps){
-    return ()
+function SingleWeatherDetail(props: SingleWeatherDetailProps) {
+  return (
+    <div className="flex flex-col justify-between gap-2 items-center text-xs font-semibold text-black/80">
+      <p className="whitespace-nowrap">{props.information}</p>
+      <div className="text-3xl">{props.icon}</div>
+      <p>{props.value}</p>
+    </div>
+  );
 }
