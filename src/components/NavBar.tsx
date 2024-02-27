775acba16038daa795ddf3661ca1de20dd70fb25
rflores <104962825+rfloresj@users.noreply.github.com>
2024-02-27 09:55:55 -0600

    SuggestionBox:props

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -67,10 +67,20 @@ export default function NavBar({}: Props) {
   );
 }
 
-function SuggestionBox() {
+function SuggestionBox({
+  showSuggestion,
+  suggestions,
+  handleSuggestionClick,
+  error,
+}: {
+  showSuggestion: boolean;
+  suggestions: string[];
+  handleSuggestionClick: (item: string) => void;
+  error: string;
+}) {
   return (
     <ul className='mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2'>
-      <li></li>
+      <li className='cursor-pointer p-1 rounded hover:bg-gray-200'></li>
     </ul>
   );
 }
