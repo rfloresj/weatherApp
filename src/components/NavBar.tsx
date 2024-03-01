b9a69be2af69754af65b81a5ca9c00fcac4c2b02
rflores <104962825+rfloresj@users.noreply.github.com>
2024-03-01 15:29:26 -0600

    NavBar:Add-props:SuggestionBox

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -64,7 +64,14 @@ export default function NavBar({}: Props) {
               // onSubmit={}
               onChange={(e) => handleInputChange(e.target.value)}
             />
-            <SuggestionBox />
+            <SuggestionBox
+              {...{
+                showSuggestions,
+                suggestions,
+                handleSuggestionClick,
+                error,
+              }}
+            />
           </div>
         </section>
       </div>
@@ -73,19 +80,19 @@ export default function NavBar({}: Props) {
 }
 
 function SuggestionBox({
-  showSuggestion,
+  showSuggestions,
   suggestions,
   handleSuggestionClick,
   error,
 }: {
-  showSuggestion: boolean;
+  showSuggestions: boolean;
   suggestions: string[];
   handleSuggestionClick: (item: string) => void;
   error: string;
 }) {
   return (
     <>
-      {((showSuggestion && suggestions.length > 1) || error) && (
+      {((showSuggestions && suggestions.length > 1) || error) && (
         <ul className='mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2'>
           {error && suggestions.length < 1 && (
             <li className='text-red.500 p-1'>{error}</li>
@@ -95,11 +102,11 @@ function SuggestionBox({
               key={i}
               onClick={() => handleSuggestionClick(item)}
               className='cursor-pointer p-1 rounded hover:bg-gray-200'
-            ></li>
+            >
+              {item}
+            </li>
           ))}
-          <li className='cursor-pointer p-1 rounded hover:bg-gray-200'>
-            {item}
-          </li>
+          <li className='cursor-pointer p-1 rounded hover:bg-gray-200'></li>
         </ul>
       )}
     </>
