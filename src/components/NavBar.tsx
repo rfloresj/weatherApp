ce2427d48d32ca22ea6a327a2637af5eec09f06c
rflores <104962825+rfloresj@users.noreply.github.com>
2024-03-21 10:17:12 -0600

    add-responsive-styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -86,40 +86,60 @@ export default function NavBar({ location }: Props) {
     }
   }
   return (
-    <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
-      <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
-        <p className='flex items-center justify-center gap-2'>
-          <h2 className='text-gray-500 text-3xl'>Weather</h2>
-          <BsSun className='text-3xl mt-1 text-yellow-400' />
-        </p>
-        {/*  */}
-        <section className='flex gap-2 items-center'>
-          <SlLocationPin
-            title='Your current location'
-            onClick={handleCurrentLocation}
-            className='text-2xl text-gray-700 hover:opacity-60 cursor-pointer'
+    <>
+      <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
+        <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
+          <p className='flex items-center justify-center gap-2'>
+            <h2 className='text-gray-500 text-3xl'>Weather</h2>
+            <BsSun className='text-3xl mt-1 text-yellow-400' />
+          </p>
+          {/*  */}
+          <section className='flex gap-2 items-center'>
+            <SlLocationPin
+              title='Your current location'
+              onClick={handleCurrentLocation}
+              className='text-2xl text-gray-700 hover:opacity-60 cursor-pointer'
+            />
+            <GrMapLocation className='text-2xl border-0' />
+            <p className='text-slate-900/80 text-sm pl-1'> {location} </p>
+            <div className='relative hidden md:flex'>
+              {/* SearchBox */}
+              <SearchBar
+                value={city}
+                onSubmit={handleSubmiSearch}
+                onChange={(e) => handleInputChange(e.target.value)}
+              />
+              <SuggestionBox
+                {...{
+                  showSuggestions,
+                  suggestions,
+                  handleSuggestionClick,
+                  error,
+                }}
+              />
+            </div>
+          </section>
+        </div>
+      </nav>
+      <section className='flex max-w-7xl px-3 md:hidden'>
+        <div className='relative '>
+          {/* SearchBox */}
+          <SearchBar
+            value={city}
+            onSubmit={handleSubmiSearch}
+            onChange={(e) => handleInputChange(e.target.value)}
           />
-          <GrMapLocation className='text-2xl border-0' />
-          <p className='text-slate-900/80 text-sm pl-1'> {location} </p>
-          <div className='relative'>
-            {/* SearchBox */}
-            <SearchBar
-              value={city}
-              onSubmit={handleSubmiSearch}
-              onChange={(e) => handleInputChange(e.target.value)}
-            />
-            <SuggestionBox
-              {...{
-                showSuggestions,
-                suggestions,
-                handleSuggestionClick,
-                error,
-              }}
-            />
-          </div>
-        </section>
-      </div>
-    </nav>
+          <SuggestionBox
+            {...{
+              showSuggestions,
+              suggestions,
+              handleSuggestionClick,
+              error,
+            }}
+          />
+        </div>
+      </section>
+    </>
   );
 }
 
